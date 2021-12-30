2d15abec014a888be569d39ecd3a953c4283fea7
Oleg <oleg.lukianov@raiffeisen.ua>
2021-12-31 00:01:24 +0200

    Fix pylint: Your code has been rated at 8.89/10 (previous run: 4.66/10)

--- delete_dublicate_mp3.py
+++ delete_dublicate_mp3.py
@@ -10,65 +10,66 @@ import re
 import sys
 import socket
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __author__ = 'Oleg Lukianov'
 
 #logging.basicConfig(level=logging.DEBUG, filename="./delete_dublicate_mp3.log")
 logging.basicConfig(level=logging.DEBUG)
 
 class ParseMusicTrack:
+    """
+    Main class
+    """
     debug_mode = False
-    hostname = socket.gethostname()
+    HOSTNAME = socket.gethostname()
 
-    if hostname == 'LK4H2Q05N':
+    if HOSTNAME == 'LK4H2Q05N':
         path_main = "/Users/iuad15au/Downloads/test"
-    elif hostname == 'localhost':
+    elif HOSTNAME == 'localhost':
         path_main = "/storage/emulated/0/Musik/Ленинград"
     else:
-        print(f'Need set var "path_main", you hostname: {hostname}')
+        print(f'Need set var "path_main", you HOSTNAME: {HOSTNAME}')
         os._exit(1)
-    
 
     def parse_all_tracks(self):
-            """
-            Documentation
-            """
-            this_function_name = cast(types.FrameType, inspect.currentframe()).f_code.co_name
-            logging.debug('Run function = %s', this_function_name)
-            all_name = []
-            path_main = self.path_main
+        """
+        Documentation
+        """
+        this_function_name = cast(types.FrameType, inspect.currentframe()).f_code.co_name
+        logging.debug('Run function = %s', this_function_name)
+        all_name = []
+        path_main = self.path_main
 
-            for root, _, files in os.walk(path_main):
-                for fname in files:
-                    path = os.path.join(root, fname)
-                    all_name.append(fname)
-            #         if self.debug_mode:
-            #             print(f'File={fname}')
-            # if self.debug_mode:
-            #     print('\n')
+        for root, _, files in os.walk(path_main):
+            for fname in files:
+                all_name.append(fname)
+        #         if self.debug_mode:
+        #             print(f'File={fname}')
+        # if self.debug_mode:
+        #     print('\n')
 
-            return all_name
+        return all_name
 
     def unique_names(self):
-            """
-            Documentation
-            """
-            this_function_name = cast(types.FrameType, inspect.currentframe()).f_code.co_name
-            logging.debug('Run function = %s', this_function_name)
-            unique_name_all = []
-            all_name = self.parse_all_tracks()
-            pattern = ".[a-z0-9]{3}$|^[0-9.]{1,4}"
+        """
+        Documentation
+        """
+        this_function_name = cast(types.FrameType, inspect.currentframe()).f_code.co_name
+        logging.debug('Run function = %s', this_function_name)
+        unique_name_all = []
+        all_name = self.parse_all_tracks()
+        pattern = ".[a-z0-9]{3}$|^[0-9.]{1,4}"
 
-            for file in all_name:
-                unique_name = re.sub(pattern, '', file)
-                unique_name = unique_name.strip()
-                unique_name_all.append(unique_name)
-                if self.debug_mode:
-                    print(f'File={file} \t\t unique_name={unique_name}')
+        for file in all_name:
+            unique_name = re.sub(pattern, '', file)
+            unique_name = unique_name.strip()
+            unique_name_all.append(unique_name)
             if self.debug_mode:
-                print('\n')
+                print(f'File={file} \t\t unique_name={unique_name}')
+        if self.debug_mode:
+            print('\n')
 
-            return set(unique_name_all)
+        return set(unique_name_all)
 
     def compare_with_tracks(self):
         """
@@ -78,24 +79,24 @@ class ParseMusicTrack:
         logging.debug('Run function = %s', this_function_name)
         unique_names = self.unique_names()
         all_name = self.parse_all_tracks()
-        for unique_name in unique_names:
-            newList = list(filter(lambda key: unique_name in key, all_name))
-            if newList.__len__() > 1:
-                print("n=" + newList.__str__() + " count: " + newList.__len__().__str__())
-        
+        for name in unique_names:
+            new_list = list(filter(lambda key: name in key, all_name))
+            if new_list.__len__() > 1:
+                print("n=" + new_list.__str__() + " count: " + new_list.__len__().__str__())
+
 
 if __name__ == "__main__":
     COUNT = None
     ARG = None
-    debug_mode = False
+    DEBUG_MODE = False
     for COUNT, ARG in enumerate(sys.argv):
         if re.search('[Dd][Ee][Bb][Uu][Gg]', ARG):
-            debug_mode = True
+            DEBUG_MODE = True
 
-    if debug_mode is True or len(sys.argv) == 1:
+    if DEBUG_MODE is True or len(sys.argv) == 1:
         parse_music_track = ParseMusicTrack()
         try:
-            parse_music_track.debug_mode = debug_mode
+            parse_music_track.debug_mode = DEBUG_MODE
             parse_music_track.compare_with_tracks()
         except KeyboardInterrupt:
             print('\nExit. User press Ctrl+C (KeyboardInterrupt)')
@@ -103,5 +104,5 @@ if __name__ == "__main__":
         print('Incorrect argument (correct only "debug")')
         print(f"Argument {COUNT:>6}: {ARG}")
         print(f"Arguments count: {len(sys.argv)}")
-        print(f'Debug mode = {debug_mode}')
-        print(f'Hostname = {socket.gethostname()}')
+        print(f'Debug mode = {DEBUG_MODE}')
+        print(f'HOSTNAME = {socket.gethostname()}')
