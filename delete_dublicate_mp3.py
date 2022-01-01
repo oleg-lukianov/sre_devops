aeb6a493232b66c82e1b5cb9112907e02f8a8f2b
Oleg <oleg.lukianov@raiffeisen.ua>
2022-01-01 18:29:02 +0200

    Add function delete files

--- delete_dublicate_mp3.py
+++ delete_dublicate_mp3.py
@@ -10,7 +10,7 @@ import re
 import sys
 import socket
 
-__version__ = '1.1.2'
+__version__ = '1.2.1'
 __author__ = 'Oleg Lukianov'
 
 #logging.basicConfig(level=logging.DEBUG, filename="./delete_dublicate_mp3.log")
@@ -22,31 +22,38 @@ class ParseMusicTrack:
     """
     debug_mode = False
     HOSTNAME = socket.gethostname()
+    list_track = {}
+    list_track_unique = []
+    list_path = {}
 
     if HOSTNAME == 'LK4H2Q05N':
-        path_main = "/Users/iuad15au/Downloads/test"
+        path_main = "/Users/iuad15au/Downloads/test2"
     elif HOSTNAME == 'localhost':
         path_main = "/storage/emulated/0/Musik/Ленинград"
     else:
         print(f'Need set var "path_main", you HOSTNAME: {HOSTNAME}')
         os._exit(1)
 
-    def parse_all_tracks(self):
+    def get_all_tracks(self):
         """
         Documentation
         """
         this_function_name = cast(types.FrameType, inspect.currentframe()).f_code.co_name
         logging.debug('Run function = %s', this_function_name)
         all_name = []
+        number = 0
         path_main = self.path_main
 
         for root, _, files in os.walk(path_main):
             for fname in files:
+                number += 1
                 all_name.append(fname)
-        #         if self.debug_mode:
-        #             print(f'File={fname}')
-        # if self.debug_mode:
-        #     print('\n')
+                self.list_track[str(number)] = fname
+                self.list_path[str(number)] = root
+                if self.debug_mode:
+                    print(f'File={fname} \t path={root} \t _={_}')
+        if self.debug_mode:
+            print('\n')
 
         return all_name
 
@@ -56,20 +63,15 @@ class ParseMusicTrack:
         """
         this_function_name = cast(types.FrameType, inspect.currentframe()).f_code.co_name
         logging.debug('Run function = %s', this_function_name)
-        unique_name_all = []
-        all_name = self.parse_all_tracks()
         pattern = ".[a-z0-9]{3}$|^[0-9.]{1,4}"
 
-        for file in all_name:
-            unique_name = re.sub(pattern, '', file)
+        for key_track in self.list_track:
+            unique_name = re.sub(pattern, '', self.list_track[key_track])
             unique_name = unique_name.strip()
-            unique_name_all.append(unique_name)
-            if self.debug_mode:
-                print(f'File={file} \t\t unique_name={unique_name}')
-        if self.debug_mode:
-            print('\n')
-
-        return set(unique_name_all)
+            if unique_name not in self.list_track_unique:
+                self.list_track_unique.append(unique_name)
+                if self.debug_mode:
+                    print(f'unique_name={unique_name}')
 
     def compare_with_tracks(self):
         """
@@ -77,13 +79,55 @@ class ParseMusicTrack:
         """
         this_function_name = cast(types.FrameType, inspect.currentframe()).f_code.co_name
         logging.debug('Run function = %s', this_function_name)
-        unique_names = self.unique_names()
-        all_name = self.parse_all_tracks()
-        for name in unique_names:
-            new_list = list(filter(lambda key: name in key, all_name))
-            if new_list.__len__() > 1:
-                print("n=" + new_list.__str__() + " count: " + new_list.__len__().__str__())
 
+        for track_unique in self.list_track_unique:
+            count = 0
+            list_track = {}
+            list_path = {}
+            if self.debug_mode:
+                print(f'track_unique = {track_unique}')
+            for key_track in self.list_track:
+                if self.debug_mode:
+                    print(f'key_track = {self.list_track[key_track]}')
+                if re.search(track_unique, self.list_track[key_track]):
+                    count += 1
+                    list_track[str(count)] = self.list_track[key_track]
+                    list_path[str(count)] = self.list_path[key_track]
+                    if self.debug_mode:
+                        print(f'unique={track_unique} \t all={self.list_track[key_track]}')
+            if self.debug_mode:
+                print(f'Number of coincidence = {count}')
+            if count > 1:
+                self.delete_file(list_track, list_path)
+
+    @staticmethod
+    def delete_file(list_track, list_path):
+        """
+        Documentation
+        """
+        this_function_name = cast(types.FrameType, inspect.currentframe()).f_code.co_name
+        logging.debug('Run function = %s', this_function_name)
+        print('~~~~Choose files to delete~~~~')
+        for num in list_track:
+            print(f'Number={num} \t track name: "{list_track[num]}" \t '
+                    f'path: "{list_path[num]}/{list_track[num]}"')
+
+        try:
+            txt = input("~~~~Insert number for delete file: ")
+            number = str(txt)
+        except KeyboardInterrupt:
+            print('\nExit. User press Ctrl+C (KeyboardInterrupt)')
+            os._exit(1)
+
+        for num in number:
+            if list_track.get(num):
+                full_path_with_file = f'{list_path[num]}/{list_track[num]}'
+                print(f'Deleted num={num} \t track name: "{full_path_with_file}"')
+                del list_track[num]
+                if os.path.exists(full_path_with_file):
+                    os.remove(full_path_with_file)
+
+        print('\n')
 
 if __name__ == "__main__":
     COUNT = None
@@ -97,7 +141,15 @@ if __name__ == "__main__":
         parse_music_track = ParseMusicTrack()
         try:
             parse_music_track.debug_mode = DEBUG_MODE
+            parse_music_track.get_all_tracks()
+            parse_music_track.unique_names()
             parse_music_track.compare_with_tracks()
+
+            if DEBUG_MODE:
+                print('\n')
+                print(parse_music_track.list_track)
+                print(parse_music_track.list_track_unique)
+                print(parse_music_track.list_path)
         except KeyboardInterrupt:
             print('\nExit. User press Ctrl+C (KeyboardInterrupt)')
     else:
---
+++ test.py
@@ -0,0 +1,9 @@
+n = ['Всё это рейв', 'The Rolling Stones', 'Х_й в пальто', 'Колбаса-любовь', 'Блюз', 'Колбаса-любовь (remix)', 'The Rolling Stones (remix)']
+n.sort()
+r = set(n)
+
+for x in n:
+    print(x)
+
+print(n)
+print(r)
